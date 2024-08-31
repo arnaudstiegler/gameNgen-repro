4fb4aaedcf9f1342270bac8f36581ab76dea90ea
Arnaud Stiegler <arnaud.stiegler@gmail.com>
2024-08-31 11:57:54 +0200

    temp
    remove file

--- stable_baselines_playground/run_trained_agent.py
+++ stable_baselines_playground/run_trained_agent.py
@@ -2,15 +2,31 @@ from loguru import logger
 from stable_baselines3 import PPO
 import gymnasium
 from vizdoom import gymnasium_wrapper
+import numpy as np
+
 
 logger.info('Start predict')
 
-env = gymnasium.make("VizdoomHealthGatheringSupreme-v0", render_mode="rgb_array")
+vizdoom_scenarios = [
+    "VizdoomHealthGatheringSupreme-v0",
+    "VizdoomCorridor-v0",
+    "VizdoomDefendCenter-v0",
+    "VizdoomHealthGathering-v0",
+    "VizdoomMyWayHome-v0",
+    "VizdoomPredictPosition-v0",
+    "VizdoomTakeCover-v0",
+]
 
-model = PPO.load("ppo_trained_agent", env=env)
+env = gymnasium.make("VizdoomDefendLine-v0", render_mode="rgb_array")
+env.get_wrapper_attr('game').set_render_hud(True)
+
+model = PPO.load("trained_agents/ppo_trained_agent", env=env)
 vec_env = model.get_env()
 obs = vec_env.reset()
 for i in range(1000):
-    action, _state = model.predict(obs, deterministic=True)
+    # Either use the model to predict the action or sample a random action
+    # action, _state = model.predict(obs, deterministic=True)
+    action = np.array(env.action_space.sample())
+    import ipdb; ipdb.set_trace()
     obs, reward, done, info = vec_env.step(action)
-    vec_env.render("human")
+    # vec_env.render("human")
--- stable_baselines_playground/train_ppo.py
+++ stable_baselines_playground/train_ppo.py
@@ -6,13 +6,13 @@ from loguru import logger
 
 from vizdoom import gymnasium_wrapper  # noqa
 
-env = gymnasium.make("VizdoomHealthGatheringSupreme-v0", render_mode="rgb_array")
+env = gymnasium.make("VizdoomDefendLine-v0", render_mode="rgb_array")
 
 logger.info('Start training')
 model = PPO("MultiInputPolicy", env, verbose=1)
-model.learn(total_timesteps=10)
+model.learn(total_timesteps=10, progress_bar=True)
 
-model.save("ppo_trained_agent")
+model.save("trained_agents/ppo_trained_agent")
 
     # VecEnv resets automatically
     # if done:
